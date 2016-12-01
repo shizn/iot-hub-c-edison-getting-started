57042841ba4fd3f726074f0186ff009aa32e9b62
ZhijunZhao <zhijun.zhao@outlook.com>
2016-12-01 19:38:18 +0800

    use node-cpplint for c code (#24)

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
 
+var cpplint = require('node-cpplint/lib/index');
+var reporter = require('node-cpplint/lib/reporters').spec
+
 gulp.task('lint', () => {
   return gulp.src([
     '**/*.js',
@@ -15,3 +18,39 @@ gulp.task('lint', () => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
+
+gulp.task('cpplint', () => {
+  var options = {
+    files: [
+      './Lesson1/app/main.c',
+      './Lesson3/app/main.c',
+      './Lesson4/app/main.c'
+    ],
+    filters: {
+      'whitespace': {
+        'braces': false,
+        'line_length': false,
+        'newline': false
+      },
+      'build': {
+        'include': false
+      }
+    },
+    extensions: [
+      'c'
+    ]
+  };
+
+  cpplint(options, function (err, report) {
+    reporter(err, report);
+
+    // Return failure if there is any error.
+    for (var key in report) {
+      if (report.hasOwnProperty(key) && Array.isArray(report[key])) {
+        if (report[key].length > 0) {
+          process.exit(1);
+        }
+      }
+    }
+  });
+})
